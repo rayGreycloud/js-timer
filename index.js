844bfbf1fb53cf6022e7436f055a742eb95d38d1
rayGreycloud <rayGreycloud@gmail.com>
2017-01-01 13:49:45 -0800

    Add display of end time

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
 
 function timer(seconds) {
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
+  displayEndTime(then);
 
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -24,4 +26,11 @@ function displayTimeLeft(seconds) {
 
   document.title = display;
   timerDisplay.textContent = display;
+}
+
+function displayEndTime(timestamp) {
+    const end = new Date(timestamp);
+    const hour = end.getHours();
+    const minutes = end.getMinutes();
+    endTime.textContent = `Ends at: ${hour > 12 ? hour -12 : hour}:${minutes < 10 ? '0' : ''}${minutes}`;
 }
