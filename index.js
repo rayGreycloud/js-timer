08b9fd04afc4abe8c4d18bdd0a817154dccaafb3
rayGreycloud <rayGreycloud@gmail.com>
2017-01-01 14:45:04 -0800

    Wire up buttons and time input

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
+
 
 function timer(seconds) {
+  clearInterval(countdown);
+
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
@@ -34,3 +38,17 @@ function displayEndTime(timestamp) {
     const minutes = end.getMinutes();
     endTime.textContent = `Ends at: ${hour > 12 ? hour -12 : hour}:${minutes < 10 ? '0' : ''}${minutes}`;
 }
+
+function startTimer() {
+  const seconds = parseInt(this.dataset.time);
+  timer(seconds);
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
+
+document.customForm.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const mins = this.minutes.value;
+  timer(mins * 60);
+  this.reset();
+});
